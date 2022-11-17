88e7234aa185c825073efb556d88fc59f56e7206
public-api ci <team-apics@twilio.com>
2022-11-17 18:02:57 +0000

    Automated client deploy (from segmentio/public-api@9e09f1c)

--- api/client.go
+++ api/client.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API. 
 
-API version: 32.0.4
+API version: 33.0.0
 Contact: friends@segment.com
 */
 
@@ -42,7 +42,7 @@ var (
 	xmlCheck  = regexp.MustCompile(`(?i:(?:application|text)/xml)`)
 )
 
-// APIClient manages communication with the Segment Public API API v32.0.4
+// APIClient manages communication with the Segment Public API API v33.0.0
 // In most cases there should be only one, shared, APIClient.
 type APIClient struct {
 	cfg    *Configuration
