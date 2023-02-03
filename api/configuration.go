ced548250aa3c6925265c9402be984caec352fc4
APIs and Common Services team <team-apics@twilio.com>
2023-02-03 00:07:30 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@562a8d0.

--- api/configuration.go
+++ api/configuration.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 34.0.0
+API version: 34.0.1
 Contact: friends@segment.com
 */
 
@@ -102,7 +102,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "Public API SDK 34.0.0 (Go)",
+		UserAgent:     "Public API SDK 34.0.1 (Go)",
 		Debug:         false,
 		Servers: ServerConfigurations{
 			{
