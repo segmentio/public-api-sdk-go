af5d82b87c9a3dc4f2ad021ad3a32e4739525880
APIs and Common Services team <team-apics@twilio.com>
2023-07-13 17:42:20 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@b13c173.

--- api/model_update_selective_sync_for_warehouse_and_space_alpha_output.go
+++ api/model_update_selective_sync_for_warehouse_and_space_alpha_output.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 35.0.0
+API version: 36.1.0
 Contact: friends@segment.com
 */
 
@@ -15,7 +15,7 @@ import (
 	"encoding/json"
 )
 
-// UpdateSelectiveSyncForWarehouseAndSpaceAlphaOutput Results from a selectiveSync patch to a Space Warehouse connection.
+// UpdateSelectiveSyncForWarehouseAndSpaceAlphaOutput Results from a SelectiveSync patch to a Space Warehouse connection.
 type UpdateSelectiveSyncForWarehouseAndSpaceAlphaOutput struct {
 	// Status of the update operation.
 	Status string `json:"status"`
