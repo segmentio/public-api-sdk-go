03b5b5c0694f5874c29d404723fe85476bf150b4
APIs and Common Services team <team-apics@twilio.com>
2023-09-29 23:10:20 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@4c6d02da.

--- api/client.go
+++ api/client.go
@@ -212,22 +212,7 @@ func typeCheckParameter(obj interface{}, expected string, name string) error {
 
 // parameterToString convert interface{} parameters to string, using a delimiter if format is provided.
 func parameterToString(obj interface{}, collectionFormat string) string {
-	var delimiter string
-
-	switch collectionFormat {
-	case "pipes":
-		delimiter = "|"
-	case "ssv":
-		delimiter = " "
-	case "tsv":
-		delimiter = "\t"
-	case "csv":
-		delimiter = ","
-	}
-
-	if reflect.TypeOf(obj).Kind() == reflect.Slice {
-		return strings.Trim(strings.Replace(fmt.Sprint(obj), " ", delimiter, -1), "[]")
-	} else if t, ok := obj.(time.Time); ok {
+	if t, ok := obj.(time.Time); ok {
 		return t.Format(time.RFC3339)
 	} else if reflect.TypeOf(obj).Kind() == reflect.String {
 		return fmt.Sprintf("%v", obj)
