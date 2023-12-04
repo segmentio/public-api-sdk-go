89d3d2fdda49731f541c5c3c6c609933c02784f5
APIs and Common Services team <team-apics@twilio.com>
2023-12-04 16:05:40 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@655411de.

--- api/model_audit_event_v1.go
+++ api/model_audit_event_v1.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 38.1.0
+API version: 38.2.0
 Contact: friends@segment.com
 */
 
@@ -28,6 +28,8 @@ type AuditEventV1 struct {
 	Type string `json:"type"`
 	// The user or API token that triggered this event.
 	Actor string `json:"actor"`
+	// The email of the user that triggered this event.
+	ActorEmail *string `json:"actorEmail,omitempty"`
 	// The identifier of the resource affected by this event.
 	ResourceId string `json:"resourceId"`
 	// The kind of resource affected by this event.
@@ -164,6 +166,38 @@ func (o *AuditEventV1) SetActor(v string) {
 	o.Actor = v
 }
 
+// GetActorEmail returns the ActorEmail field value if set, zero value otherwise.
+func (o *AuditEventV1) GetActorEmail() string {
+	if o == nil || IsNil(o.ActorEmail) {
+		var ret string
+		return ret
+	}
+	return *o.ActorEmail
+}
+
+// GetActorEmailOk returns a tuple with the ActorEmail field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *AuditEventV1) GetActorEmailOk() (*string, bool) {
+	if o == nil || IsNil(o.ActorEmail) {
+		return nil, false
+	}
+	return o.ActorEmail, true
+}
+
+// HasActorEmail returns a boolean if a field has been set.
+func (o *AuditEventV1) HasActorEmail() bool {
+	if o != nil && !IsNil(o.ActorEmail) {
+		return true
+	}
+
+	return false
+}
+
+// SetActorEmail gets a reference to the given string and assigns it to the ActorEmail field.
+func (o *AuditEventV1) SetActorEmail(v string) {
+	o.ActorEmail = &v
+}
+
 // GetResourceId returns the ResourceId field value
 func (o *AuditEventV1) GetResourceId() string {
 	if o == nil {
@@ -250,6 +284,9 @@ func (o AuditEventV1) ToMap() (map[string]interface{}, error) {
 	toSerialize["timestamp"] = o.Timestamp
 	toSerialize["type"] = o.Type
 	toSerialize["actor"] = o.Actor
+	if !IsNil(o.ActorEmail) {
+		toSerialize["actorEmail"] = o.ActorEmail
+	}
 	toSerialize["resourceId"] = o.ResourceId
 	toSerialize["resourceType"] = o.ResourceType
 	toSerialize["resourceName"] = o.ResourceName
