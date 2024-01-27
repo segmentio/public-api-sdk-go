d82659621648896e52e3ee4b8ea27662cf64b0c8
APIs and Common Services team <team-apics@twilio.com>
2024-01-27 01:01:18 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@bd0789fc.

--- api/api_destinations.go
+++ api/api_destinations.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 38.5.0
+API version: 39.0.0
 Contact: friends@segment.com
 */
 
@@ -94,6 +94,7 @@ func (a *DestinationsAPIService) CreateDestinationExecute(
 	// to determine the Content-Type header
 	localVarHTTPContentTypes := []string{
 		"application/vnd.segment.v1+json",
+		"application/json",
 		"application/vnd.segment.v1beta+json",
 		"application/vnd.segment.v1alpha+json",
 	}
@@ -972,8 +973,6 @@ Based on the granularity chosen, there are restrictions on the time range you ca
 - Max time range: 14 days
 - Oldest possible start time: 14 days in the past
 
-The rate limit for this endpoint is 60 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.
-
 	@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
 	@param destinationId
 	@return ApiListDeliveryMetricsSummaryFromDestinationRequest
@@ -1757,6 +1756,7 @@ func (a *DestinationsAPIService) UpdateDestinationExecute(
 	// to determine the Content-Type header
 	localVarHTTPContentTypes := []string{
 		"application/vnd.segment.v1+json",
+		"application/json",
 		"application/vnd.segment.v1beta+json",
 		"application/vnd.segment.v1alpha+json",
 	}
--- api/api_iam_groups.go
+++ api/api_iam_groups.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 38.5.0
+API version: 39.0.0
 Contact: friends@segment.com
 */
 
@@ -107,6 +107,7 @@ func (a *IAMGroupsAPIService) AddPermissionsToUserGroupExecute(
 	// to determine the Content-Type header
 	localVarHTTPContentTypes := []string{
 		"application/vnd.segment.v1+json",
+		"application/json",
 		"application/vnd.segment.v1beta+json",
 		"application/vnd.segment.v1alpha+json",
 	}
@@ -298,6 +299,7 @@ func (a *IAMGroupsAPIService) AddUsersToUserGroupExecute(
 	// to determine the Content-Type header
 	localVarHTTPContentTypes := []string{
 		"application/vnd.segment.v1+json",
+		"application/json",
 		"application/vnd.segment.v1beta+json",
 		"application/vnd.segment.v1alpha+json",
 	}
@@ -477,6 +479,7 @@ func (a *IAMGroupsAPIService) CreateUserGroupExecute(
 	// to determine the Content-Type header
 	localVarHTTPContentTypes := []string{
 		"application/vnd.segment.v1+json",
+		"application/json",
 		"application/vnd.segment.v1beta+json",
 		"application/vnd.segment.v1alpha+json",
 	}
@@ -1716,6 +1719,7 @@ func (a *IAMGroupsAPIService) ReplacePermissionsForUserGroupExecute(
 	// to determine the Content-Type header
 	localVarHTTPContentTypes := []string{
 		"application/vnd.segment.v1+json",
+		"application/json",
 		"application/vnd.segment.v1beta+json",
 		"application/vnd.segment.v1alpha+json",
 	}
@@ -1908,6 +1912,7 @@ func (a *IAMGroupsAPIService) ReplaceUsersInUserGroupExecute(
 	// to determine the Content-Type header
 	localVarHTTPContentTypes := []string{
 		"application/vnd.segment.v1+json",
+		"application/json",
 		"application/vnd.segment.v1beta+json",
 		"application/vnd.segment.v1alpha+json",
 	}
@@ -2098,6 +2103,7 @@ func (a *IAMGroupsAPIService) UpdateUserGroupExecute(
 	// to determine the Content-Type header
 	localVarHTTPContentTypes := []string{
 		"application/vnd.segment.v1+json",
+		"application/json",
 		"application/vnd.segment.v1beta+json",
 		"application/vnd.segment.v1alpha+json",
 	}
