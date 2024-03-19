ef7eb170a4c7f386142b249354d5c34fab61a7d4
APIs and Common Services team <team-apics@twilio.com>
2024-03-19 16:09:14 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@127e9335.

--- api/model_delivery_overview_filter_by.go
+++ api/model_delivery_overview_filter_by.go
@@ -20,7 +20,7 @@ var _ MappedNullable = &DeliveryOverviewFilterBy{}
 
 // DeliveryOverviewFilterBy The `DeliveryOverviewFilterBy` object is a map of the filterable fields and their values.
 type DeliveryOverviewFilterBy struct {
-	// A list of strings of discard reasons. Can be used to filter all Source and Destination steps, except for Successfully Received. If you would like to view retry attempts for a successful delivery, you can filter from `successes.attempt.1` to `successes.attempt.10`.  See Discard Record Documentation for valid error codes.
+	// A list of strings of discard reasons. Can be used to filter all Source and Destination steps, except for Successfully Received. If you would like to view retry attempts for a successful delivery, you can filter from `successes.attempt.1` to `successes.attempt.10`.  See [Discard Record Documentation](https://segment.com/docs/connections/delivery-overview/#troubleshooting) for valid error codes.
 	DiscardReason []string `json:"discardReason,omitempty"`
 	// A list of strings of event names.
 	EventName []string `json:"eventName,omitempty"`
