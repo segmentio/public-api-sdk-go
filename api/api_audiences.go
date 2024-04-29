3acd185a162cb18295ebe95985d547977eb76917
APIs and Common Services team <team-apics@twilio.com>
2024-04-29 20:48:14 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@873734cb.

--- api/api_audiences.go
+++ api/api_audiences.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 49.0.0
+API version: 50.0.0
 Contact: friends@segment.com
 */
 
@@ -22,6 +22,190 @@ import (
 
 // AudiencesAPIService AudiencesAPI service
 type AudiencesAPIService service
+
+type ApiCreateAudienceRequest struct {
+	ctx                      context.Context
+	ApiService               *AudiencesAPIService
+	spaceId                  string
+	createAudienceAlphaInput *CreateAudienceAlphaInput
+}
+
+func (r ApiCreateAudienceRequest) CreateAudienceAlphaInput(
+	createAudienceAlphaInput CreateAudienceAlphaInput,
+) ApiCreateAudienceRequest {
+	r.createAudienceAlphaInput = &createAudienceAlphaInput
+	return r
+}
+
+func (r ApiCreateAudienceRequest) Execute() (*CreateAudience200Response, *http.Response, error) {
+	return r.ApiService.CreateAudienceExecute(r)
+}
+
+/*
+CreateAudience Create Audience
+
+Creates Audience.
+
+• This endpoint is in **Alpha** testing.  Please submit any feedback by sending email to friends@segment.com.
+
+• In order to successfully call this endpoint, the specified Workspace needs to have the Audience feature enabled. Please reach out to your customer success manager for more information.
+
+	@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	@param spaceId
+	@return ApiCreateAudienceRequest
+*/
+func (a *AudiencesAPIService) CreateAudience(
+	ctx context.Context,
+	spaceId string,
+) ApiCreateAudienceRequest {
+	return ApiCreateAudienceRequest{
+		ApiService: a,
+		ctx:        ctx,
+		spaceId:    spaceId,
+	}
+}
+
+// Execute executes the request
+//
+//	@return CreateAudience200Response
+func (a *AudiencesAPIService) CreateAudienceExecute(
+	r ApiCreateAudienceRequest,
+) (*CreateAudience200Response, *http.Response, error) {
+	var (
+		localVarHTTPMethod  = http.MethodPost
+		localVarPostBody    interface{}
+		formFiles           []formFile
+		localVarReturnValue *CreateAudience200Response
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(
+		r.ctx,
+		"AudiencesAPIService.CreateAudience",
+	)
+	if err != nil {
+		return localVarReturnValue, nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/spaces/{spaceId}/audiences"
+	localVarPath = strings.Replace(
+		localVarPath,
+		"{"+"spaceId"+"}",
+		url.PathEscape(parameterValueToString(r.spaceId, "spaceId")),
+		-1,
+	)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+	if r.createAudienceAlphaInput == nil {
+		return localVarReturnValue, nil, reportError(
+			"createAudienceAlphaInput is required and must be specified",
+		)
+	}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{"application/vnd.segment.v1alpha+json"}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{
+		"application/vnd.segment.v1alpha+json",
+		"application/json",
+	}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	// body params
+	localVarPostBody = r.createAudienceAlphaInput
+	req, err := a.client.prepareRequest(
+		r.ctx,
+		localVarPath,
+		localVarHTTPMethod,
+		localVarPostBody,
+		localVarHeaderParams,
+		localVarQueryParams,
+		localVarFormParams,
+		formFiles,
+	)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := io.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = io.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 404 {
+			var v RequestErrorEnvelope
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 422 {
+			var v RequestErrorEnvelope
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 429 {
+			var v RequestErrorEnvelope
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+			newErr.model = v
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.client.decode(
+		&localVarReturnValue,
+		localVarBody,
+		localVarHTTPResponse.Header.Get("Content-Type"),
+	)
+	if err != nil {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
 
 type ApiGetAudienceRequest struct {
 	ctx        context.Context
--- api/model_get_audience_alpha_output.go
+++ api/model_get_audience_alpha_output.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 49.0.0
+API version: 50.0.0
 Contact: friends@segment.com
 */
 
@@ -18,7 +18,7 @@ import (
 // checks if the GetAudienceAlphaOutput type satisfies the MappedNullable interface at compile time
 var _ MappedNullable = &GetAudienceAlphaOutput{}
 
-// GetAudienceAlphaOutput Audience output for update.
+// GetAudienceAlphaOutput Audience output for get.
 type GetAudienceAlphaOutput struct {
 	Audience AudienceSummary `json:"audience"`
 }
