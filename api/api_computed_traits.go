72802bffe13ff9ba2426e993e4168a62fb69aa1f
APIs and Common Services team <team-apics@twilio.com>
2024-05-22 19:15:55 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@c81c9eee.

--- api/api_computed_traits.go
+++ api/api_computed_traits.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 50.1.0
+API version: 50.2.0
 Contact: friends@segment.com
 */
 
@@ -50,7 +50,9 @@ CreateComputedTrait Create Computed Trait
 
 • In order to successfully call this endpoint, the specified Workspace needs to have the Computed Trait feature enabled. Please reach out to your customer success manager for more information.
 
-• When called, this endpoint may generate the `Computed Trait Created` event in the [audit trail](/tag/Audit-Trail). Note: The definition for a Computed Trait created via the API is not editable via the Segment App.
+• When called, this endpoint may generate the `Computed Trait Created` event in the [audit trail](/tag/Audit-Trail).\
+
+	Note: The definition for a Computed Trait created via the API is not editable via the Segment App.
 
 The rate limit for this endpoint is 10 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.
 
