97f77fcc25ae87d2c04956e081a6db2bb67f6cc9
APIs and Common Services team <team-apics@twilio.com>
2024-09-09 23:57:35 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@3363268f.

--- api/model_reverse_etl_schedule_config.go
+++ api/model_reverse_etl_schedule_config.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 53.3.0
+API version: 54.1.0
 Contact: friends@segment.com
 */
 
@@ -12,6 +12,7 @@ Contact: friends@segment.com
 package api
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 )
@@ -25,8 +26,14 @@ type ReverseEtlScheduleConfig struct {
 // Unmarshal JSON data into any of the pointers in the struct
 func (dst *ReverseEtlScheduleConfig) UnmarshalJSON(data []byte) error {
 	var err error
+
+	decoder := json.NewDecoder(bytes.NewReader(data))
+	decoder.DisallowUnknownFields()
+
 	// try to unmarshal JSON data into ReverseEtlPeriodicScheduleConfig
-	err = json.Unmarshal(data, &dst.ReverseEtlPeriodicScheduleConfig)
+	decoder = json.NewDecoder(bytes.NewReader(data))
+	decoder.DisallowUnknownFields()
+	err = decoder.Decode(&dst.ReverseEtlPeriodicScheduleConfig)
 	if err == nil {
 		jsonReverseEtlPeriodicScheduleConfig, _ := json.Marshal(
 			dst.ReverseEtlPeriodicScheduleConfig,
@@ -41,7 +48,9 @@ func (dst *ReverseEtlScheduleConfig) UnmarshalJSON(data []byte) error {
 	}
 
 	// try to unmarshal JSON data into ReverseEtlSpecificTimeScheduleConfig
-	err = json.Unmarshal(data, &dst.ReverseEtlSpecificTimeScheduleConfig)
+	decoder = json.NewDecoder(bytes.NewReader(data))
+	decoder.DisallowUnknownFields()
+	err = decoder.Decode(&dst.ReverseEtlSpecificTimeScheduleConfig)
 	if err == nil {
 		jsonReverseEtlSpecificTimeScheduleConfig, _ := json.Marshal(
 			dst.ReverseEtlSpecificTimeScheduleConfig,
