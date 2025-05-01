ce180d18bddf36ae43c67463f554e31f1790b3ca
APIs and Common Services team <team-apics@twilio.com>
2025-05-01 20:37:49 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@39a662fa.

--- api/model_get_audience_beta_output.go
+++ api/model_get_audience_beta_output.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 58.3.0
+API version: 58.2.0
 Contact: friends@segment.com
 */
 
@@ -20,16 +20,14 @@ var _ MappedNullable = &GetAudienceBetaOutput{}
 
 // GetAudienceBetaOutput Audience output for get.
 type GetAudienceBetaOutput struct {
-	Audience AudienceSummaryWithAudienceTypeAndLookback `json:"audience"`
+	Audience AudienceSummary `json:"audience"`
 }
 
 // NewGetAudienceBetaOutput instantiates a new GetAudienceBetaOutput object
 // This constructor will assign default values to properties that have it defined,
 // and makes sure properties required by API are set, but the set of arguments
 // will change when the set of required properties is changed
-func NewGetAudienceBetaOutput(
-	audience AudienceSummaryWithAudienceTypeAndLookback,
-) *GetAudienceBetaOutput {
+func NewGetAudienceBetaOutput(audience AudienceSummary) *GetAudienceBetaOutput {
 	this := GetAudienceBetaOutput{}
 	this.Audience = audience
 	return &this
@@ -44,9 +42,9 @@ func NewGetAudienceBetaOutputWithDefaults() *GetAudienceBetaOutput {
 }
 
 // GetAudience returns the Audience field value
-func (o *GetAudienceBetaOutput) GetAudience() AudienceSummaryWithAudienceTypeAndLookback {
+func (o *GetAudienceBetaOutput) GetAudience() AudienceSummary {
 	if o == nil {
-		var ret AudienceSummaryWithAudienceTypeAndLookback
+		var ret AudienceSummary
 		return ret
 	}
 
@@ -55,7 +53,7 @@ func (o *GetAudienceBetaOutput) GetAudience() AudienceSummaryWithAudienceTypeAnd
 
 // GetAudienceOk returns a tuple with the Audience field value
 // and a boolean to check if the value has been set.
-func (o *GetAudienceBetaOutput) GetAudienceOk() (*AudienceSummaryWithAudienceTypeAndLookback, bool) {
+func (o *GetAudienceBetaOutput) GetAudienceOk() (*AudienceSummary, bool) {
 	if o == nil {
 		return nil, false
 	}
@@ -63,7 +61,7 @@ func (o *GetAudienceBetaOutput) GetAudienceOk() (*AudienceSummaryWithAudienceTyp
 }
 
 // SetAudience sets field value
-func (o *GetAudienceBetaOutput) SetAudience(v AudienceSummaryWithAudienceTypeAndLookback) {
+func (o *GetAudienceBetaOutput) SetAudience(v AudienceSummary) {
 	o.Audience = v
 }
 
