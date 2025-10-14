80ad1635a31dba690b9fe2328c9a780003aba6c7
APIs and Common Services team <team-apics@twilio.com>
2025-10-14 18:09:45 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@51b17f8e.

--- api/model_add_audience_schedule_to_audience_alpha_input.go
+++ api/model_add_audience_schedule_to_audience_alpha_input.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 61.0.0
+API version: 60.1.2
 Contact: friends@segment.com
 */
 
@@ -20,7 +20,7 @@ var _ MappedNullable = &AddAudienceScheduleToAudienceAlphaInput{}
 
 // AddAudienceScheduleToAudienceAlphaInput Defines an input for creating an audience schedule.
 type AddAudienceScheduleToAudienceAlphaInput struct {
-	// Strategy of the audience schedule (manual, periodic, or specific days).
+	// Strategy of the audience schedule (periodic or specific days).
 	Strategy string         `json:"strategy"`
 	Config   NullableConfig `json:"config,omitempty"`
 }
