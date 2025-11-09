bef5fba2962b8bce6c0dd8df00e7277ae13a436c
APIs and Common Services team <team-apics@twilio.com>
2025-11-09 20:35:48 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@ff987602.

--- api/api_functions.go
+++ api/api_functions.go
@@ -541,6 +541,180 @@ func (a *FunctionsAPIService) CreateInsertFunctionInstanceExecute(
 	return localVarReturnValue, localVarHTTPResponse, nil
 }
 
+type ApiCreateTransformationFunctionInstanceRequest struct {
+	ctx                                            context.Context
+	ApiService                                     *FunctionsAPIService
+	createTransformationFunctionInstanceAlphaInput *CreateTransformationFunctionInstanceAlphaInput
+}
+
+func (r ApiCreateTransformationFunctionInstanceRequest) CreateTransformationFunctionInstanceAlphaInput(
+	createTransformationFunctionInstanceAlphaInput CreateTransformationFunctionInstanceAlphaInput,
+) ApiCreateTransformationFunctionInstanceRequest {
+	r.createTransformationFunctionInstanceAlphaInput = &createTransformationFunctionInstanceAlphaInput
+	return r
+}
+
+func (r ApiCreateTransformationFunctionInstanceRequest) Execute() (*CreateTransformationFunctionInstance200Response, *http.Response, error) {
+	return r.ApiService.CreateTransformationFunctionInstanceExecute(r)
+}
+
+/*
+CreateTransformationFunctionInstance Create Transformation Function Instance
+
+Creates a Transformation Function instance connected to the given Integration.
+
+This endpoint is specifically designed for Transformations and includes integration_type as a mandatory field.
+
+• In order to successfully call this endpoint, the specified Workspace needs to have the Functions feature enabled. Please reach out to your customer success manager for more information.
+
+	@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	@return ApiCreateTransformationFunctionInstanceRequest
+*/
+func (a *FunctionsAPIService) CreateTransformationFunctionInstance(
+	ctx context.Context,
+) ApiCreateTransformationFunctionInstanceRequest {
+	return ApiCreateTransformationFunctionInstanceRequest{
+		ApiService: a,
+		ctx:        ctx,
+	}
+}
+
+// Execute executes the request
+//
+//	@return CreateTransformationFunctionInstance200Response
+func (a *FunctionsAPIService) CreateTransformationFunctionInstanceExecute(
+	r ApiCreateTransformationFunctionInstanceRequest,
+) (*CreateTransformationFunctionInstance200Response, *http.Response, error) {
+	var (
+		localVarHTTPMethod  = http.MethodPost
+		localVarPostBody    interface{}
+		formFiles           []formFile
+		localVarReturnValue *CreateTransformationFunctionInstance200Response
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(
+		r.ctx,
+		"FunctionsAPIService.CreateTransformationFunctionInstance",
+	)
+	if err != nil {
+		return localVarReturnValue, nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/transformation-function-instances"
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+	if r.createTransformationFunctionInstanceAlphaInput == nil {
+		return localVarReturnValue, nil, reportError(
+			"createTransformationFunctionInstanceAlphaInput is required and must be specified",
+		)
+	}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{"application/vnd.segment.v1alpha+json"}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{
+		"application/vnd.segment.v1alpha+json",
+		"application/json",
+	}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	// body params
+	localVarPostBody = r.createTransformationFunctionInstanceAlphaInput
+	req, err := a.client.prepareRequest(
+		r.ctx,
+		localVarPath,
+		localVarHTTPMethod,
+		localVarPostBody,
+		localVarHeaderParams,
+		localVarQueryParams,
+		localVarFormParams,
+		formFiles,
+	)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := io.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = io.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 404 {
+			var v RequestErrorEnvelope
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 422 {
+			var v RequestErrorEnvelope
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 429 {
+			var v RequestErrorEnvelope
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+			newErr.model = v
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.client.decode(
+		&localVarReturnValue,
+		localVarBody,
+		localVarHTTPResponse.Header.Get("Content-Type"),
+	)
+	if err != nil {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
 type ApiDeleteFunctionRequest struct {
 	ctx        context.Context
 	ApiService *FunctionsAPIService
