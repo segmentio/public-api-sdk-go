8d9e7f26b4cd84f7f5a5e609277eb5d70cfa1cd6
APIs and Common Services team <team-apics@twilio.com>
2025-11-27 16:54:15 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@5a20ebdf.

--- api/api_deletion_and_suppression.go
+++ api/api_deletion_and_suppression.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 62.1.0
+API version: 62.0.2
 Contact: friends@segment.com
 */
 
@@ -462,6 +462,8 @@ Rate limit headers will be updated to reflect regulation-specific limits (tracke
   - Segment & Destination Regulations: DELETE_ONLY, SUPPRESS_WITH_DELETE
 
 - X-RateLimit-Reset: RFC 5322 timestamp for when the quota resets (for example, Tue, 31 Dec 2024 23:59:59 GMT)
+
+The rate limit for this endpoint is 5 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.
 
 	@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
 	@return ApiCreateWorkspaceRegulationRequest
