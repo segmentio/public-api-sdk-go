0624146113482a2a11b03d36992ff37af340685c
APIs and Common Services team <team-apics@twilio.com>
2026-01-27 21:46:18 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@2ad6441f.

--- api/api_audiences.go
+++ api/api_audiences.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 63.0.0
+API version: 64.0.0
 Contact: friends@segment.com
 */
 
@@ -2044,9 +2044,10 @@ func (a *AudiencesAPIService) RemoveAudienceFromSpaceExecute(
 
 	// to determine the Accept header
 	localVarHTTPHeaderAccepts := []string{
+		"application/vnd.segment.v1+json",
+		"application/json",
 		"application/vnd.segment.v1beta+json",
 		"application/vnd.segment.v1alpha+json",
-		"application/json",
 	}
 
 	// set Accept header
--- api/model_remove_audience_from_space_200_response_1.go
+++ api/model_remove_audience_from_space_200_response_1.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 63.0.0
+API version: 64.0.0
 Contact: friends@segment.com
 */
 
@@ -20,7 +20,7 @@ var _ MappedNullable = &RemoveAudienceFromSpace200Response1{}
 
 // RemoveAudienceFromSpace200Response1 struct for RemoveAudienceFromSpace200Response1
 type RemoveAudienceFromSpace200Response1 struct {
-	Data *RemoveAudienceFromSpaceAlphaOutput `json:"data,omitempty"`
+	Data *RemoveAudienceFromSpaceBetaOutput `json:"data,omitempty"`
 }
 
 // NewRemoveAudienceFromSpace200Response1 instantiates a new RemoveAudienceFromSpace200Response1 object
@@ -41,9 +41,9 @@ func NewRemoveAudienceFromSpace200Response1WithDefaults() *RemoveAudienceFromSpa
 }
 
 // GetData returns the Data field value if set, zero value otherwise.
-func (o *RemoveAudienceFromSpace200Response1) GetData() RemoveAudienceFromSpaceAlphaOutput {
+func (o *RemoveAudienceFromSpace200Response1) GetData() RemoveAudienceFromSpaceBetaOutput {
 	if o == nil || IsNil(o.Data) {
-		var ret RemoveAudienceFromSpaceAlphaOutput
+		var ret RemoveAudienceFromSpaceBetaOutput
 		return ret
 	}
 	return *o.Data
@@ -51,7 +51,7 @@ func (o *RemoveAudienceFromSpace200Response1) GetData() RemoveAudienceFromSpaceA
 
 // GetDataOk returns a tuple with the Data field value if set, nil otherwise
 // and a boolean to check if the value has been set.
-func (o *RemoveAudienceFromSpace200Response1) GetDataOk() (*RemoveAudienceFromSpaceAlphaOutput, bool) {
+func (o *RemoveAudienceFromSpace200Response1) GetDataOk() (*RemoveAudienceFromSpaceBetaOutput, bool) {
 	if o == nil || IsNil(o.Data) {
 		return nil, false
 	}
@@ -67,8 +67,8 @@ func (o *RemoveAudienceFromSpace200Response1) HasData() bool {
 	return false
 }
 
-// SetData gets a reference to the given RemoveAudienceFromSpaceAlphaOutput and assigns it to the Data field.
-func (o *RemoveAudienceFromSpace200Response1) SetData(v RemoveAudienceFromSpaceAlphaOutput) {
+// SetData gets a reference to the given RemoveAudienceFromSpaceBetaOutput and assigns it to the Data field.
+func (o *RemoveAudienceFromSpace200Response1) SetData(v RemoveAudienceFromSpaceBetaOutput) {
 	o.Data = &v
 }
 
