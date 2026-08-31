5de9e56e9162b2b6137243bd1af68d359989ce88
APIs and Common Services team <team-apics@twilio.com>
2026-08-31 13:28:48 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@e89c1ed7.

--- api/api_activations.go
+++ api/api_activations.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 73.2.0
+API version: 73.3.0
 Contact: friends@segment.com
 */
 
@@ -341,6 +341,190 @@ func (a *ActivationsAPIService) AddDestinationToAudienceExecute(
 	}
 	// body params
 	localVarPostBody = r.addDestinationToAudienceAlphaInput
+	req, err := a.client.prepareRequest(
+		r.ctx,
+		localVarPath,
+		localVarHTTPMethod,
+		localVarPostBody,
+		localVarHeaderParams,
+		localVarQueryParams,
+		localVarFormParams,
+		formFiles,
+	)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := io.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = io.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 404 {
+			var v RequestErrorEnvelope
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 422 {
+			var v RequestErrorEnvelope
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 429 {
+			var v RequestErrorEnvelope
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+			newErr.model = v
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.client.decode(
+		&localVarReturnValue,
+		localVarBody,
+		localVarHTTPResponse.Header.Get("Content-Type"),
+	)
+	if err != nil {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
+type ApiBatchQueryActivationsForSpaceRequest struct {
+	ctx                                     context.Context
+	ApiService                              *ActivationsAPIService
+	spaceId                                 string
+	batchQueryActivationsForSpaceAlphaInput *BatchQueryActivationsForSpaceAlphaInput
+}
+
+func (r ApiBatchQueryActivationsForSpaceRequest) BatchQueryActivationsForSpaceAlphaInput(
+	batchQueryActivationsForSpaceAlphaInput BatchQueryActivationsForSpaceAlphaInput,
+) ApiBatchQueryActivationsForSpaceRequest {
+	r.batchQueryActivationsForSpaceAlphaInput = &batchQueryActivationsForSpaceAlphaInput
+	return r
+}
+
+func (r ApiBatchQueryActivationsForSpaceRequest) Execute() (*BatchQueryActivationsForSpace200Response, *http.Response, error) {
+	return r.ApiService.BatchQueryActivationsForSpaceExecute(r)
+}
+
+/*
+BatchQueryActivationsForSpace Batch Query Activations for Space
+
+Lists Activations in bulk across many Audiences in one call. Intended for bulk reads, for example pulling activation metadata for hundreds of audiences on a schedule, so callers don't need one `listActivationsFromAudience` call per audience.
+
+• In order to successfully call this endpoint, the specified Workspace needs to have the Audience feature enabled. Please reach out to your customer success manager for more information.
+
+The rate limit for this endpoint is 60 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.
+
+	@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	@param spaceId
+	@return ApiBatchQueryActivationsForSpaceRequest
+*/
+func (a *ActivationsAPIService) BatchQueryActivationsForSpace(
+	ctx context.Context,
+	spaceId string,
+) ApiBatchQueryActivationsForSpaceRequest {
+	return ApiBatchQueryActivationsForSpaceRequest{
+		ApiService: a,
+		ctx:        ctx,
+		spaceId:    spaceId,
+	}
+}
+
+// Execute executes the request
+//
+//	@return BatchQueryActivationsForSpace200Response
+func (a *ActivationsAPIService) BatchQueryActivationsForSpaceExecute(
+	r ApiBatchQueryActivationsForSpaceRequest,
+) (*BatchQueryActivationsForSpace200Response, *http.Response, error) {
+	var (
+		localVarHTTPMethod  = http.MethodPost
+		localVarPostBody    interface{}
+		formFiles           []formFile
+		localVarReturnValue *BatchQueryActivationsForSpace200Response
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(
+		r.ctx,
+		"ActivationsAPIService.BatchQueryActivationsForSpace",
+	)
+	if err != nil {
+		return localVarReturnValue, nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/spaces/{spaceId}/activations/batch"
+	localVarPath = strings.Replace(
+		localVarPath,
+		"{"+"spaceId"+"}",
+		url.PathEscape(parameterValueToString(r.spaceId, "spaceId")),
+		-1,
+	)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+	if r.batchQueryActivationsForSpaceAlphaInput == nil {
+		return localVarReturnValue, nil, reportError(
+			"batchQueryActivationsForSpaceAlphaInput is required and must be specified",
+		)
+	}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{"application/vnd.segment.v1alpha+json"}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{
+		"application/vnd.segment.v1alpha+json",
+		"application/json",
+	}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	// body params
+	localVarPostBody = r.batchQueryActivationsForSpaceAlphaInput
 	req, err := a.client.prepareRequest(
 		r.ctx,
 		localVarPath,
