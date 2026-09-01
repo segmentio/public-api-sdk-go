82d239af9e090fcfa8fd24781772f6db946e65e1
APIs and Common Services team <team-apics@twilio.com>
2026-09-01 17:26:38 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@3f84ddb6.

--- api/api_audiences.go
+++ api/api_audiences.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 73.4.1
+API version: 73.4.0
 Contact: friends@segment.com
 */
 
@@ -1773,6 +1773,8 @@ ListAudienceConsumersFromSpaceAndAudience List Audience Consumers from Space And
 
 Returns the list of consumers for the given audience.
 
+• This endpoint is in **Alpha** testing.  Please submit any feedback by sending an email to friends@segment.com.
+
 • In order to successfully call this endpoint, the specified Workspace needs to have the Audience feature enabled. Please reach out to your customer success manager for more information.
 
 The rate limit for this endpoint is 25 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.
@@ -1854,9 +1856,8 @@ func (a *AudiencesAPIService) ListAudienceConsumersFromSpaceAndAudienceExecute(
 
 	// to determine the Accept header
 	localVarHTTPHeaderAccepts := []string{
-		"application/vnd.segment.v1+json",
-		"application/json",
 		"application/vnd.segment.v1alpha+json",
+		"application/json",
 	}
 
 	// set Accept header
--- api/model_list_audience_consumers_from_space_and_audience_200_response.go
+++ api/model_list_audience_consumers_from_space_and_audience_200_response.go
@@ -3,7 +3,7 @@ Segment Public API
 
 The Segment Public API helps you manage your Segment Workspaces and its resources. You can use the API to perform CRUD (create, read, update, delete) operations at no extra charge. This includes working with resources such as Sources, Destinations, Warehouses, Tracking Plans, and the Segment Destinations and Sources Catalogs.  All CRUD endpoints in the API follow REST conventions and use standard HTTP methods. Different URL endpoints represent different resources in a Workspace.  See the next sections for more information on how to use the Segment Public API.
 
-API version: 73.4.1
+API version: 73.4.0
 Contact: friends@segment.com
 */
 
@@ -20,7 +20,7 @@ var _ MappedNullable = &ListAudienceConsumersFromSpaceAndAudience200Response{}
 
 // ListAudienceConsumersFromSpaceAndAudience200Response struct for ListAudienceConsumersFromSpaceAndAudience200Response
 type ListAudienceConsumersFromSpaceAndAudience200Response struct {
-	Data *ListAudienceConsumersFromSpaceAndAudienceOutput `json:"data,omitempty"`
+	Data *ListAudienceConsumersFromSpaceAndAudienceAlphaOutput `json:"data,omitempty"`
 }
 
 // NewListAudienceConsumersFromSpaceAndAudience200Response instantiates a new ListAudienceConsumersFromSpaceAndAudience200Response object
@@ -41,9 +41,9 @@ func NewListAudienceConsumersFromSpaceAndAudience200ResponseWithDefaults() *List
 }
 
 // GetData returns the Data field value if set, zero value otherwise.
-func (o *ListAudienceConsumersFromSpaceAndAudience200Response) GetData() ListAudienceConsumersFromSpaceAndAudienceOutput {
+func (o *ListAudienceConsumersFromSpaceAndAudience200Response) GetData() ListAudienceConsumersFromSpaceAndAudienceAlphaOutput {
 	if o == nil || IsNil(o.Data) {
-		var ret ListAudienceConsumersFromSpaceAndAudienceOutput
+		var ret ListAudienceConsumersFromSpaceAndAudienceAlphaOutput
 		return ret
 	}
 	return *o.Data
@@ -51,7 +51,7 @@ func (o *ListAudienceConsumersFromSpaceAndAudience200Response) GetData() ListAud
 
 // GetDataOk returns a tuple with the Data field value if set, nil otherwise
 // and a boolean to check if the value has been set.
-func (o *ListAudienceConsumersFromSpaceAndAudience200Response) GetDataOk() (*ListAudienceConsumersFromSpaceAndAudienceOutput, bool) {
+func (o *ListAudienceConsumersFromSpaceAndAudience200Response) GetDataOk() (*ListAudienceConsumersFromSpaceAndAudienceAlphaOutput, bool) {
 	if o == nil || IsNil(o.Data) {
 		return nil, false
 	}
@@ -67,9 +67,9 @@ func (o *ListAudienceConsumersFromSpaceAndAudience200Response) HasData() bool {
 	return false
 }
 
-// SetData gets a reference to the given ListAudienceConsumersFromSpaceAndAudienceOutput and assigns it to the Data field.
+// SetData gets a reference to the given ListAudienceConsumersFromSpaceAndAudienceAlphaOutput and assigns it to the Data field.
 func (o *ListAudienceConsumersFromSpaceAndAudience200Response) SetData(
-	v ListAudienceConsumersFromSpaceAndAudienceOutput,
+	v ListAudienceConsumersFromSpaceAndAudienceAlphaOutput,
 ) {
 	o.Data = &v
 }
